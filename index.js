8f64227673a84fc83d069da5c6beb12174fe677a
Jennifer Dobak <jdobak1@gmail.com>
2017-09-30 03:00:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  var spy=string
+  console.log(spy.toUpperCase())
+}
+function logWhisper(string) {
+  var spy=string
+  console.log(spy.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  var lowercase=string
+  if (lowercase.toLowerCase() === lowercase) {
+      return 'I can\'t hear you!'
+}}
